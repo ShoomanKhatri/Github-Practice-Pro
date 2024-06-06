0e43d85d4c821b37a4815014f1b2c78833f2f45b
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-06-06 08:34:34 +0545

    Update index.js
    Co-Authored-By: Laxmi-KC <159879082+Laxmi-KC@users.noreply.github.com>
    Co-Authored-By: indra-kc <159883430+indra-kc@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //extraordinare
 console.log("hello world"); //prints helo world
-let x="1";
-console.log(x+1);  // this will return 11.
- 
-let y ="1"
-console.log(x+y);
+let x = "1";
+// console.log(x+1);
+// this will return 11.
+
+let y = "1";
+console.log(x + y);
+//this will return 11
